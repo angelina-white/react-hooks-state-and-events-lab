174ed90ecb9eb8a747a7d2450eaf4faee3c7f484
Angelina <angelinarwhite@gmail.com>
2022-05-19 16:37:38 -0400

    do deliverables

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [isOn, setIsOn] = useState(false);
+
+  function addToCart() 
+  {
+    setIsOn((isOn) => !isOn);
+  }
+
   return (
     <li className="">
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={addToCart} className= {isOn ? "in-cart" : "add"}>{isOn ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
